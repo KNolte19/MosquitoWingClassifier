c5db65e13aeded557f68b0ad7b8914448a3353f6
KNolte19 <60223645+KNolte19@users.noreply.github.com>
2024-02-06 12:17:50 +0100

    Update image_processing.py

--- image_processing.py
+++ image_processing.py
@@ -2,7 +2,6 @@ import pandas as pd
 import numpy as np
 import tensorflow as tf
 import os 
-import keras_cv
 
 from skimage.measure import label, regionprops_table
 from rembg import remove, new_session
