6f2f6a58a762c1a0bf098169942247852d0953bb
KNolte19 <60223645+KNolte19@users.noreply.github.com>
2024-12-06 15:34:51 +0100

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-mkdir -p ~/MosquitoWingClassifier/app/static/requests
-chown -R appuser:appuser ~/MosquitoWingClassifier/app/static/requests
+mkdir -p ~/MosquitoWingClassifier/static/requests
+chown -R appuser:appuser ~/MosquitoWingClassifier/static/requests
 
 exec gunicorn app:app -b 0.0.0.0:8080 --timeout 360
 
