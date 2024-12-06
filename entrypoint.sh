f1493d1ccd93c5ae1969e6b26093f72f94ec1ad4
KNolte19 <60223645+KNolte19@users.noreply.github.com>
2024-12-06 14:52:45 +0100

    Adress Permission Problems

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-mkdir -p /app/static/requests
-chown -R appuser:appuser /app/static/requests
+mkdir -p ~/MosquitoWingClassifier/app/static/requests
+chown -R appuser:appuser ~/MosquitoWingClassifier/app/static/requests
 
-exec gunicorn app:app -b 0.0.0.0:8080 --timeout 120
+exec gunicorn app:app -b 0.0.0.0:8080 --timeout 360
 
